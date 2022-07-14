03b26b1d15c9ed24145df269da7251ab46243db9
Rodolphe <92471439+RodolpheFmd@users.noreply.github.com>
2022-07-14 11:18:34 +0100

    Add files via upload

---
+++ MARL_plugin.py
@@ -0,0 +1,106 @@
+""" By Rodolphe Fremond - PhD Student, Cranfield University 2022
+    Supervisor: Dr. Yan Xu, Dr. Gokhan Inalhan
+    Tactical En-Route Solver : Model 1 trainer
+    Multi-agent Reinforcement Learning :
+    Advantage Actor Critic based on Proximal Policy Optimisation
+"""
+
+from Multi_agent.Airspace_scenario import generate_airspace, generate_route
+from Multi_agent.env_info import Agent, reset, update_observation
+import Multi_agent.settings as set
+if set.device_spe:
+    from Multi_agent.PPO_device import SuperAgent
+else:
+    from Multi_agent.PPO import SuperAgent
+from datetime import datetime
+import numpy as np
+import random, os
+
+AI = SuperAgent()
+AI.memory.episode = 1
+origin, destination = generate_airspace()
+
+def init_plugin():
+    global agents, step, start, traffic_flow, cnt_flow, agents_in_traf, origin, destination, ac_remaining, safeness, safeness_list
+    traffic_flow, step, cnt_flow, ac_remaining, safeness, safeness_list = random.randint(15, 30), 0, 0, set.population-1, 0, []
+    print(f'\nNew episode : {AI.memory.episode}')
+    if set.scenario==1:
+        agents = [Agent("UAS" + str(k + 1), random.randint(10, 35), origin[k%2], destination) for k in range(set.population)]
+    if set.scenario==2:
+        agents = [Agent("UAS" + str(k + 1), random.randint(10, 35), origin[k%2], destination[k%2]) for k in range(set.population)]
+    if set.scenario==3:
+        routes = [generate_route() for k in range(set.population)]
+        agents = [Agent("UAS" + str(k + 1), random.randint(10, 35), routes[k][0], routes[k][1]) for k in range(set.population)]
+    agents_in_traf = [agents[0]]
+    start = datetime.now()
+    agents_in_traf[-1].generate
+
+    print(f'Simulation starts at {start}')
+    config = {
+        'plugin_name': 'MARL_plugin',
+        'plugin_type': 'sim',
+        'update_interval': 5,
+        'update': update,
+        'preupdate': preupdate}
+    return config
+
+def preupdate():
+    pass
+
+def update():
+    global agents, start, step, traffic_flow, cnt_flow, agents_in_traf, ac_remaining, safeness, safeness_list
+
+    step += 1
+    cnt_flow += 1
+
+    if cnt_flow == traffic_flow and ac_remaining!=0:
+        cnt_flow, traffic_flow = 0, random.randint(10,30)
+        agents_in_traf.append(agents[set.population-ac_remaining])
+        agents_in_traf[-1].generate
+        ac_remaining -= 1
+
+    for agent in agents_in_traf:
+        agent.update_info
+    for agent in agents_in_traf:
+        if agent.target_reached:
+            AI.remember(agent)
+            agent.delete_agent
+            agents_in_traf.remove(agent)
+
+    if (len(agents_in_traf)==0 and ac_remaining==0) or (step >= set.max_iteration):
+        if step >= set.max_iteration:
+            for agent in agents_in_traf:
+                AI.remember(agent)
+                agent.delete_agent
+                agents_in_traf.remove(agent)
+        if AI.memory.episode == set.max_episode:
+            reset()
+            if set.shut_down:
+                os.system("shutdown /s /t 1")
+            return
+
+        print(f'Number of conflict : {AI.memory.conflicts}')
+        print(f'Number of crash : {AI.memory.crashs}')
+        print(f'Current safeness : {safeness} Nm without any collision')
+        if len(safeness_list)>0:
+            print(f'Model safeness : {1/(sum(safeness_list)/len(safeness_list))} collision/Nm')
+        print(f'Operating time of the episode {AI.memory.episode} : {datetime.now()-start}')
+
+        print(f'\nReinitialisation of the parameters')
+        AI.memory.clear_memory
+        init_plugin()
+        return
+
+    update_observation(agents_in_traf)
+
+    for agent in agents_in_traf:
+        agent.update_observation_state
+        agent.tactical_mode_update
+        AI.choose_action(agent)
+        agent.perform_action
+        agent.save_trajectory
+        if np.prod(np.array([agent.safely_flown for agent in agents_in_traf])):
+            safeness += sum([(agent.distflown-agent.last_distflown)/1852 for agent in agents_in_traf])
+        else:
+            safeness_list.append(safeness)
+            safeness = 0
---
+++ README.md
@@ -0,0 +1,45 @@
+# Application of an Autonomous Multi-agent System Using Proximal Policy Optimisation for Tactical deconfliction Within the Urban Airspace. 
+## Digital Avionics Systems Conference 2022.
+This paper complements the conference paper presented for the DASC22 by providing trained multi-agent reinforcement learning models highlighted through the experimental part. 
+This program is shared for personal, academic or performance comparison purposes.
+
+## Requirements
+### BlueSky open-source software and packages
+[BlueSky](https://github.com/TUDelft-CNS-ATM/bluesky) ATM environment hosts this application and its installation is required with a special attention to the different required Python [lbraries](https://github.com/TUDelft-CNS-ATM/bluesky/wiki/Installation) in addition to:
+- random.
+- torch.
+
+### File management
+1. *Multi_agent* folder in the original *bluesky-master* folder/
+2. Move *MARL_plugin* python file from *Multi_agent* folder to bluesky-master *plugins* folder.
+3. Move *run_MARL* script file from *Multi_agent* folder to bluesky-master *scenario* folder.
+
+### Settings
+From *settings* python file in *Multi_agent* folder, make settings modification by specifying the operating system, the population of aircraft to generate for each episode, the directory of bluesky simulator, the maximum of iteration and the scenario.
+
+## Scenarios
+### Reminder
+The conference paper presents three case studies:
+- **Case study 1** : 2 fixed routes toward a merging point.
+- **Case study 2** : 2 fixed routes intersecting at half way of their respective route.
+- **Case study 2'** : 2 non-fixed routes intersecting at half way of their respective route with the generation of random agent dynamic model.
+
+*However, this case study 2' presents here only Mavic drone model in contrast to the paper and the results from different models highlighted in the set-up section*
+
+### Choice of the scenario
+From *settings* python file in *Multi_agent* folder, change the variable 'scenario' from the integer 1, 2 or 3 respectively.
+
+### Run the scenario !
+After completing the bullets from **Requirements**, start BlueSky.py and write the command *IC run_MARL* in the bluesky simulator stack window.
+
+## Performance
+### Data collection
+Tensorboard can be easily integrated to extract performance such as the conflict number, crashes, or globally the safeness of the models
+Another method is to collect the information in a csv or xlsx file.
+
+### Assessment
+From the provided version, the performance are shown in the terminal for each episode that includes:
+- Number of conflicts : considers the cumulative number of conflict for each decision step (every 5seconds). The conflicting separation is set to 0.9Nm.
+- Number of crashes : considers the cumulative number of crashes for each decision step (every 5seconds). The crashes is over-estimated to 0.3Nm.
+- Safeness : How long (distance Nm) the agents are safely flying, this information is cumulating throughout the simulation and returns to 0 in case of unsafe separation. An additional information of the safeness is provided by giving a quantitative value of the robustness of the system highlighting the system failure (crash case) per Nm of flight.
+
