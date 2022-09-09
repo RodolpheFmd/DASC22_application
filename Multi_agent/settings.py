ef2bd14a1c3372c3b4107a08812c00222d317c05
Rodolphe <92471439+RodolpheFmd@users.noreply.github.com>
2022-09-09 11:28:16 +0100

    Update settings.py

--- Multi_agent/settings.py
+++ Multi_agent/settings.py
@@ -1,9 +1,9 @@
 '''Change settings here'''
 
-population    = 16      # Manage the number of agents performing the scenario.
+population    = 15      # Manage the number of agents performing the scenario.
 max_episode   = 20000   # Ceiling episode.
 max_iteration = 4000    # Maximum of decision step before recovering episode.
-path_save     = '/home/rodolphefremond/Desktop/BlueSky_simulator/' # Path of BlueSky simulator
-scenario      = 2       # Data collection marker
+path_save     = '/home/rodolphefremond/Desktop/bluesky-master/' # Path of BlueSky simulator
+scenario      = 1       # Data collection marker
 device_spe    = False   # Load specific device performance
 os            = 'L'     # 'W' if windows machine or 'L' for Linux
